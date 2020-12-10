fc83a719fe72fc35a51a2c97b3c5282906f821e2
KennyDwiCh <70674797+KennyDwiCh@users.noreply.github.com>
2020-12-11 06:15:15 +0700

    Pass + JWT

--- Pertemuan 12/passport.js
+++ Pertemuan 12/passport.js
@@ -1,61 +1,24 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-
-const app = express();
-
-app.get("/api" , (req ,res ) => {
-    res.json({
-        message : "Coba JWT API",
-    });
-});
-
-app.post("/api/posts", verifyToken, (req, res) => {
-    jwt.verify(req.token, "rahasia", (err, authData) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        res.json({
-          message: "post dibuat",
-          authData,
-        });
-      }
-    });
-  });
-
-  app.post("/api/login", (req, res) => {
-    //user data
-    const user = {
-      id: 5,
-      username: "Otome KAibu",
-      email: "Yapapapa.com",
-    };
-    jwt.sign({ user: user }, "rahasia", (err, token) => {
-      res.json({
-        token,
-      });
-    });
-  });
-  
-
-
-// fungsi untuk Verifikasi
-function verifyToken(req, res, next) {
-    //get header
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      // split header
-      const bearer = bearerHeader.split(" ");
-      //get token
-      const bearerToken = bearer[1];
-      //set token
-      req.token = bearerToken;
-      //next middleware
-      next();
-    } else {
-      //unauthorization
-      res.sendStatus(403);
-    }
-  }
-  
-
-app.listen(3000, () => console.log(`server berjalan di port 3000`));
+const passports = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const model = require("../models/User");
+module.exports = function(passport){
+    passport.use(
+        new passports({usernameField:"email"},(email,password,done)=>{
+            model.findOne({email:email})
+            .then((user)=>{
+                if(!user){
+                    return done(null,false,{message:"Email Tak Terdaftar"});
+                }
+                bcrypt.compare(password,user.password,(err,isMatch)=>{
+                    if(err) throw err;
+                    if(isMatch){
+                        return done(null,user);
+                    }else{
+                        return done(null,false,{message:"Password Salah"});
+                    }
+                });
+            }).catch((err)=> console.log(err));
+        })
+    )};
+    
